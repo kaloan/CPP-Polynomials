58cfed4d130ec57890827d8faf3181af0ccfe3f0
Калоян Стоилов <kkstoilov@uni-sofia.bg>
2021-08-06 16:56:05 +0300

    Add cosine example to main

--- CPP-Polynomials/main.cpp
+++ CPP-Polynomials/main.cpp
@@ -1,4 +1,5 @@
 #include "Polynomial.cpp"
+#include <math.h>
 using namespace std;
 
 int main()
@@ -7,7 +8,7 @@ int main()
 	initP[0] = 2.0;
 	initP[1] = 1.0;
 	Polynomial<double, double> p(initP);
-	cout << p.at(1.0) << " " << p.at(50.0);
+	cout << p.at(1.0) << " " << p.at(50.0) << endl;
 
 	Polynomial<int, int> intPoly;
 	intPoly[1] = 4;
@@ -34,7 +35,18 @@ int main()
 	cout << "After filling coeffs for intPoly: ";
 	intPoly.print();
 	
-	cout << intPoly.horner(2);
+	cout << "intPoly(2) by horner: " << intPoly.horner(2) << endl;
+
+
+	//You can define your own approximations to analytic functions
+	Polynomial<double, double> myCos;
+	double facts = 1;
+	for (unsigned int i = 0; i < 20; i += 2)
+	{
+		myCos[i] = 1 / facts;
+		facts *= - (int)((i + 1) * (i + 2));
+	}
+	cout << "Build in cos: " << cos(1) << " vs my cos: " << myCos.at(1) << endl;
 
 	cin.get();
 	return 0;
